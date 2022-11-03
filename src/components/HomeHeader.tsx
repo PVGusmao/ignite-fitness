dae54ac3b1d5896f1f49c6105ab2a737c1b27b31
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-03 18:11:29 -0300

    ...

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -14,6 +14,7 @@ export function HomeHeader() {
         source={{ uri: 'https://github.com/PVGusmao.png' }}
         alt='Imagem do usupario'
         size={16}
+        
       />
 
       <VStack>
