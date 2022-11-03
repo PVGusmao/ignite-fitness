031ca78d216742bfa2e6e301c70bf471ddd6c777
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-03 14:05:58 -0300

    implementado navegação entre telas de signin e signup com das devidas tipagens

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -6,13 +9,20 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1}}
       showsVerticalScrollIndicator={false}
     >
-      <VStack bg="gray.700" flex={1} px={10}>
+      <VStack flex={1} px={10}>
         <Image
+          defaultSource={BackgroundImg}
           source={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode='contain'
@@ -58,6 +68,7 @@ export function SignIn() {
           </Text>
 
           <Button
+            onPress={handleNewAccount}
             title='Criar Conta'
             variant='outline'
           />
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
@@ -7,13 +8,20 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1}}
       showsVerticalScrollIndicator={false}
     >
-      <VStack bg="gray.700" flex={1} px={10}>
+      <VStack flex={1} px={10}>
         <Image
+          defaultSource={BackgroundImg}
           source={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode='contain'
@@ -53,6 +61,7 @@ export function SignUp() {
         </Center>
 
           <Button
+            onPress={handleGoBack}
             mt={24}
             title='Voltar para o login'
             variant='outline'
