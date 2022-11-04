ccc762246afed8cc1fc51d16fa5da1e81e28a7a3
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-04 09:15:26 -0300

    criado header de grupamentos musculares

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { VStack, Text, HStack, FlatList } from 'native-base';
+
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack, Text } from 'native-base';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+  const [groups, setGroups] = useState(['costa', 'ombro', 'bíceps', 'tríceps'])
+  
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
