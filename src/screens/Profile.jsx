d9bfa5528e5441eb3528822fc67ef97ab6ab2bc4
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-04 15:37:39 -0300

    finalizado seleção da foto de usuário

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
-import { Center, ScrollView, Text, VStack, Skeleton, Heading } from 'native-base';
+import { Center, ScrollView, Text, VStack, Skeleton, Heading, useToast } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -12,6 +14,43 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/PVGusmao.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+      setPhotoIsLoading(true)
+      try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if(photoSelected.cancelled) {
+        return;
+      }
+
+      if (photoSelected.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 3) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de atpe 3MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+        setUserPhoto(photoSelected.uri);
+      }
+  
+    } catch(error) {
+      throw error;
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -29,12 +68,14 @@ export function Profile() {
             endColor='gray.400'
           /> :
           <UserPhoto
-            source={{ uri: 'https://github.com/PVGusmao.png'}}
+            source={{ uri: userPhoto }}
             alt='Imagem de Perfil'
             size={PHOTO_SIZE}
           />}
 
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={handleUserPhotoSelect}
+          >
             <Text
               color='green.500'
               fontWeight='bold'
