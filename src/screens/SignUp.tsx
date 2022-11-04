7a8c17ad68ade093730a8956a1518bab033d3feb
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-04 18:19:39 -0300

    implementado useForm com validação de input e css para saber se input eh valido ou nao

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
+import { useForm, Controller } from 'react-hook-form';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
@@ -7,11 +8,24 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+type FormDataProps = {
+  name: string,
+  email: string,
+  password: string,
+  password_confirm: string;
+}
+
 export function SignUp() {
   const navigation = useNavigation();
 
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
+
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log(data);
   }
 
   return (
@@ -40,22 +54,74 @@ export function SignUp() {
             Crie sua conta
           </Heading>
         
-          <Input
-            placeholder='Nome'
+          <Controller 
+            control={control}
+            name='name'
+            rules={{
+              required: 'Informe o nome.'
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Input
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
+          <Controller 
+            control={control}
+            name='email'
+            rules={{
+              required: 'Informe o e-mail',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='E-mail'
+                keyboardType='email-address'
+                autoCapitalize='none'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
-          
-          <Input
-            placeholder='Senha'
-            secureTextEntry
+
+          <Controller 
+            control={control}
+            name='password'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+        
+        <Controller 
+            control={control}
+            name='password_confirm'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Confirme a senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType='send'
+              />
+            )}
           />
 
           <Button
+          onPress={handleSubmit(handleSignUp)}
             title='Criar e acessar'
           />
         </Center>
